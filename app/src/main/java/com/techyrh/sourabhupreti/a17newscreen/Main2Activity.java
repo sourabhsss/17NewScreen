d06b7aa48ad37146fb7acab87efeb504b3b082bb
Sourabh Upreti <sourabhupreti16@gmail.com>
2018-12-18 23:26:23 +0530

    Initial commit

---
+++ app/src/main/java/com/techyrh/sourabhupreti/a17newscreen/Main2Activity.java
@@ -0,0 +1,19 @@
+package com.techyrh.sourabhupreti.a17newscreen;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+    public void screen1(View view){
+        Intent intent= new Intent(Main2Activity.this,MainActivity.class);
+        startActivity(intent);
+    }
+}
